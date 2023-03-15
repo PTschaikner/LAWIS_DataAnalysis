51ea62cf985a752f88a564aa0d4336cddc1b9195
PTschaikner <philipp.tschaikner@gmail.com>
2023-03-15 16:20:59 +0100

    Basic Map Functionality

--- D3/avalanchemap.js
+++ D3/avalanchemap.js
@@ -1,27 +1,49 @@
-var myMap = L.map('map').setView([47.259659, 11.400375], 9);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 18,
-}).addTo(myMap);
 
-d3.csv('avalanche-incidents.csv', function(error, data) {
-  if (error) throw error;
 
-  var markers = L.markerClusterGroup();
+var map = null;
+var zoomLevel = 13;
+var paris = new L.LatLng(47.259659, 11.400375);
 
-  var colorScale = d3.scaleOrdinal()
-    .domain(['Low', 'Moderate', 'Considerable', 'High', 'Extreme'])
-    .range(['green', 'yellow', 'orange', 'red', 'purple']);
+function showMap() {
+  initMap();
+  loadCSVData();
+}
 
-  data.forEach(function(d) {
-    var marker = L.circleMarker([d.latitude, d.longitude], {
-      radius: 10,
-      fillColor: colorScale(d.severity),
-      fillOpacity: 0.7,
-      stroke: false,
+function initMap() {
+  var tileLayer = createTileLayer();
+  var mapOptions = {
+    center: paris,
+    zoom: zoomLevel,
+    layers: [tileLayer]
+  };
+  map = new L.Map('leaflet-map', mapOptions);
+ }
+   function createTileLayer() {
+    var tileSourceURL = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}';
+    var tileSourceOptions = {
+     attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+    };
+    return new L.TileLayer(tileSourceURL, tileSourceOptions);
+  }
+
+  function loadCSVData() {
+    d3.csv("http://localhost:8000/avalanche_data.csv", function(data) {
+      data.forEach(function (avalanche) {
+        addMarker(avalanche);
+      });
     });
-    markers.addLayer(marker);
-  });
+  }
+  
+  function addMarker(avalanche) {
+    var radius = 20;
+    var color = 'green';
+    if (avalanche.involved_dead > 0) {
+      color = 'red';
+    }
+    L.circle([avalanche.location_latitude, avalanche.location_longitude], radius, {
+    color: color,
+    fillColor: color})
+      .addTo(map);
+  }
 
-  myMap.addLayer(markers);
-});
+  
