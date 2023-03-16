a29ac659f719be2044d0477b09d2bfdaaa07fc3d
PTschaikner <philipp.tschaikner@gmail.com>
2023-03-16 11:42:37 +0100

    Animated Point Appearance in D3

--- D3/avalanchemap.js
+++ D3/avalanchemap.js
@@ -1,7 +1,7 @@
 
 
 var map = null;
-var zoomLevel = 13;
+var zoomLevel = 8;
 var paris = new L.LatLng(47.259659, 11.400375);
 
 function showMap() {
@@ -14,10 +14,19 @@ function initMap() {
   var mapOptions = {
     center: paris,
     zoom: zoomLevel,
-    layers: [tileLayer]
+    layers: [tileLayer],
+    dragging: false,   // disable map dragging
+    touchZoom: false,  // disable touch zoom
+    zoomControl: false, // disable zoom buttons
+    scrollWheelZoom: false
   };
   map = new L.Map('leaflet-map', mapOptions);
- }
+  
+  // Add SVG layer to map for d3 markers
+  map._initPathRoot();
+  map.svg = d3.select('#leaflet-map').select('svg');
+}
+
    function createTileLayer() {
     var tileSourceURL = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}';
     var tileSourceOptions = {
@@ -28,22 +37,35 @@ function initMap() {
 
   function loadCSVData() {
     d3.csv("http://localhost:8000/avalanche_data.csv", function(data) {
-      data.forEach(function (avalanche) {
-        addMarker(avalanche);
+      var delay = 10; // delay in milliseconds between each marker
+      data.forEach(function (avalanche, i) {
+        setTimeout(function() {
+          addMarker(avalanche);
+        }, i * delay);
       });
     });
   }
   
   function addMarker(avalanche) {
-    var radius = 20;
-    var color = 'green';
+    var radius = 2;
+    var opacity = 0.4;
+    var strokeWidth = 0.5;
+    var color = 'gray';
     if (avalanche.involved_dead > 0) {
       color = 'red';
+      opacity = 0.8;
     }
-    L.circle([avalanche.location_latitude, avalanche.location_longitude], radius, {
-    color: color,
-    fillColor: color})
-      .addTo(map);
+    var marker = map.svg.append('circle')
+    .attr('cx', map.latLngToLayerPoint([avalanche.location_latitude, avalanche.location_longitude]).x)
+    .attr('cy', map.latLngToLayerPoint([avalanche.location_latitude, avalanche.location_longitude]).y)
+    .attr('r', 0)
+    .style('fill', color)
+    .style('stroke', 'black')
+    .style('stroke-width', strokeWidth)
+    .style('opacity', 0)
+    .transition()
+      .duration(500)
+      .attr('r', radius)
+      .style('opacity', opacity);
   }
-
   
