4926954b6985c9baaa6e4086392cacd7404dd9cc
PTschaikner <philipp.tschaikner@gmail.com>
2023-03-22 12:02:28 +0100

    created basic interactivity, buggy

--- D3/interactivemap.js
+++ D3/interactivemap.js
@@ -37,3 +37,62 @@ function initMap() {
     return new L.TileLayer(tileSourceURL, tileSourceOptions);
   }
 
+function loadCSVData () {
+  d3.csv("avalanche_data.csv").then(function (data) {
+    addMarker(data);
+  })
+}
+
+function addMarker(data) {
+  console.log(data);
+  // Get the danger rating levels in the data
+  var dangerLevels = Array.from(new Set(data.map(d => d.danger_rating_level)));
+
+  // Add checkboxes for each danger rating level
+  d3.select('body')
+    .selectAll('.checkbox')
+    .data(dangerLevels)
+    .enter()
+    .append('label')
+    .text(d => d)
+    .append('input')
+    .attr('type', 'checkbox')
+    .property('checked', true)
+    .on('change', updateMarkers);
+
+  // Add all the markers initially
+  var markers = map.svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d) { return map.latLngToLayerPoint([d.location_latitude, d.location_longitude]).x; })
+    .attr('cy', function(d) { return map.latLngToLayerPoint([d.location_latitude, d.location_longitude]).y; })
+    .attr('r', function(d) { return d.involved_dead; })
+    .attr('fill', 'red')
+    .attr('opacity', 0.5)
+    .on('mouseover', function(d) { d3.select(this).attr('opacity', 1); })
+    .on('mouseout', function(d) { d3.select(this).attr('opacity', 0.5); })
+    .append('title')
+    .text(function(d) { return d.tooltip; });
+
+    function updateMarkers() {
+      console.log('Updating markers...');
+    
+      // Get the danger rating levels that are checked and convert them to numbers
+      var checkedLevels = d3.selectAll('input[type=checkbox]:checked').nodes().map(d => d.parentNode.textContent.trim());
+      console.log(checkedLevels);
+      map.svg.selectAll('circle')
+      .data(data.filter(d => checkedLevels.includes(d.danger_rating_level)))
+      .transition() 
+      .duration(500)
+      .attr('r', function(d) { return d.involved_dead; });
+      
+    
+      // Hide the markers that are not in the filtered data
+      map.svg.selectAll('circle')
+        .data(data.filter(d => !checkedLevels.includes(d.danger_rating_level)))
+        .transition() // Add a transition to fade out the markers
+        .duration(500)
+        .attr('r', 0);
+    }
+}
